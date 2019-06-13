b9a01b844010b0d84dc7f8d6ba157a1dcdfc2b8a
-tristan- <tristan.linehan@gmail.com>
2019-06-13 13:02:27 -0700

    added more shortcuts for style components in <Block> for flex, added client brand colors in Colors.js, added devBorder option for development

--- components/Block.js
+++ components/Block.js
@@ -3,12 +3,20 @@ import { StyleSheet, View } from 'react-native';
 
 export default class Block extends Component {
     render() {
-        const { flex, center, middle, style, children, ...props } = this.props;
+        const { flex, center, middle, bottom, nowrap, wrap, column, row, stretch, devBorder, style, children, ...props } = this.props;
         const blockStyles = [
             styles.block,
             flex && { flex },
             center && styles.center,
             middle && styles.middle,
+            bottom && styles.bottom,
+            nowrap && styles.nowrap,
+            wrap && styles.wrap,
+            column && styles.column,
+            row && styles.row,
+            stretch && styles.stretch,
+            devBorder && styles.devBorder,
+            style
         ];
 
         return (
@@ -20,6 +28,7 @@ export default class Block extends Component {
 }
 
 const styles = StyleSheet.create({
+    
     block: {
         flex: 1,
     },
@@ -28,5 +37,30 @@ const styles = StyleSheet.create({
     },
     middle: {
         justifyContent: 'center',
+    },
+    bottom: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+    },
+    nowrap: {
+        flexWrap: 'nowrap'
+    },
+    wrap: {
+        flexWrap: 'wrap'
+    },
+    column: {
+        flexDirection: 'column'
+    }, 
+    row: {
+        flexDirection: 'row'
+    },
+    stretch: {
+        alignItems: 'stretch'
+    },
+    devBorder: {
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: 'red',
+        borderStyle: 'dashed',
     },
 });
