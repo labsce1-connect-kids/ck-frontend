0a1dd358c4619a408e2bdd43312d76342076e9eb
astro11 <aquila.fullstack@gmail.com>
2019-06-14 02:30:15 -0400

    Added Menu Nav

--- navigation/BestPractices.js
+++ navigation/BestPractices.js
@@ -1,15 +1,27 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation';
-
+import { Image } from 'react-native';
 import BestPractices from '../screens/BestPractices';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default createStackNavigator({
     BestPractices,
 }, 
 {
     navigationOptions: {
-        drawerLabel: 'Best Practices'
+        drawerLabel: 'Best Practices',
     },
-    defaultNavigationOptions: {
-        title: 'Best Practices'
-    }
+    defaultNavigationOptions: ({ navigation }) => ({
+        title: 'Best Practices',
+        headerStyle: {
+            borderBottomColor: 'transparent',
+        },
+        headerRight: 
+            <TouchableOpacity onPress={ () => navigation.toggleDrawer() }>
+                <Image
+                    source={require('../assets/images/menu-icon.png')}
+                    style={{ width: 23, height: 20 , marginHorizontal: 25 }}
+                />
+            </TouchableOpacity>
+    })
 },)
--- navigation/Support.js
+++ navigation/Support.js
@@ -1,16 +1,27 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation';
-
+import { Image } from 'react-native';
 import Support from '../screens/Support';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default createStackNavigator({
     Support,
 },
 {
     navigationOptions: {
-        drawerLabel: 'Support'
+        drawerLabel: 'Support',
     },
-    defaultNavigationOptions: {
-        title: 'Support'
-    }
-},  
-)
+    defaultNavigationOptions: ({ navigation }) => ({
+        title: 'Support',
+        headerStyle: {
+            borderBottomColor: 'transparent',
+        },
+        headerRight: 
+            <TouchableOpacity onPress={ () => navigation.toggleDrawer() }>
+                <Image
+                    source={require('../assets/images/menu-icon.png')}
+                    style={{ width: 23, height: 20 , marginHorizontal: 25 }}
+                />
+            </TouchableOpacity>
+    })
+},)
