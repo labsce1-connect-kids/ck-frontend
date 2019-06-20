371a44e73c153de8f774ee567f08455b00d2c29e
astro11 <aquila.fullstack@gmail.com>
2019-06-20 19:26:44 -0400

    modified search results data by adding modal functionality

---
+++ screens/SearchCardModal.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {Modal, Text, TouchableHighlight, View, Alert} from 'react-native';
+
+class ModalExample extends Component {
+
+
+  render() {
+    return (
+      <View style={{marginTop: 22}}>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 22}}>
+            <View>
+              <Text>Hello World!</Text>
+                {/* name */}
+                {/* <Text style={styles.name}>
+                    {`${item.names[0].display}`}
+                </Text> */}
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableHighlight
+          onPress={() => {
+            this.setModalVisible(true);
+          }}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+export default ModalExample;
